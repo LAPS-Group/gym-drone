d73334b82d861fc55c162310967b2633daf905e7
Zantox <vetle_er_her@hotmail.com>
2020-04-03 05:02:08 +0200

    Rendering, both notebook and rgb_array, fixed
      - Notebook now has a first in first out queue for the path, so it is
        more clear which path the drone is taking.
      - rgb_array now returns an uint8 array instead, as to fit with what
        seems to be the pseudo standard for rgb_arrays.

--- gym_drone/envs/drone_cardinal_directions.py
+++ gym_drone/envs/drone_cardinal_directions.py
@@ -4,10 +4,27 @@ from gym.utils import seeding
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Path(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, point):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        else:
+            for i in range(self.capacity - 1):
+                self.memory[i] = self.memory[i + 1]
+        self.memory[self.position] = point
+        self.position = min(self.position + 1, self.capacity - 1)
+
 def lattice_path_length(a, b):
     a_x, a_y = a
     b_x, b_y = b
     return abs(a_x - b_x) + abs(a_y - b_y)
+
 
 def create_grid(shape):
     grid = np.zeros(shape)
@@ -16,13 +33,15 @@ def create_grid(shape):
             grid[row][column] = lattice_path_length((0, 0), (row, column))
     return grid
 
+
 def pick_random_point(np_random, shape):
     rows, columns = shape
     return (np_random.choice(rows), np_random.choice(columns))
 
+
 class DroneCardinalDirectionsEnv(gym.Env):
     metadata = {'render.modes': ['human', 'notebook']}
-    
+
     def __init__(self, **kwargs):
         rows = 8
         columns = 8
@@ -33,7 +52,7 @@ class DroneCardinalDirectionsEnv(gym.Env):
         self._shape = (rows, columns)
         self._rows = rows
         self._columns = columns
-        
+
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Tuple((
             spaces.Discrete(rows),
@@ -46,7 +65,7 @@ class DroneCardinalDirectionsEnv(gym.Env):
         #state itself.
         #Gym soccer also uses spaces.Box with shape=1 to
         #process floats as action_space
-        
+
         self._drone_pos = None
         self._goal_pos = None
         self._grid = create_grid((rows, columns))
@@ -55,7 +74,7 @@ class DroneCardinalDirectionsEnv(gym.Env):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
-        
+
     def step(self, action):
         """
         Parameters
@@ -99,10 +118,10 @@ class DroneCardinalDirectionsEnv(gym.Env):
             drone_y, drone_x = self._drone_pos
             reward -= 1
         # If the drone is still inside the grid, add
-        # the current node to the list of points to 
+        # the current node to the list of points to
         # be rendered when rendering path.
         else:
-            self._path.append(self._drone_pos)
+            self._path.push(self._drone_pos)
 
         # Reward is at any point (except goal) the negative
         # of the height at each point.
@@ -113,58 +132,77 @@ class DroneCardinalDirectionsEnv(gym.Env):
         if self._drone_pos == self._goal_pos:
             reward = 0
             episode_over = True
-        
+
         return self._get_obs(), reward, episode_over, {}
 
     def _get_obs(self):
         return np.array([self._drone_pos[0], self._drone_pos[1],
-                self._goal_pos[0], self._goal_pos[1]])
-        
+                         self._goal_pos[0], self._goal_pos[1]])
+
     def reset(self):
         self._drone_pos = pick_random_point(self.np_random, self._shape)
         self._goal_pos = self._drone_pos
         while self._goal_pos == self._drone_pos:
             self._goal_pos = pick_random_point(self.np_random, self._shape)
-        self._path = [self._drone_pos]
+        # self._path = [self._drone_pos]
+        self._path = Path(5)
+        self._path.push(self._drone_pos)
         return self._get_obs()
-        
+
     def render(self, mode='notebook'):
         if mode == 'notebook':
             plt.imshow(self._grid)
             plt.axis("off")
             y, x = ([] for i in range(2))
-            for node_y, node_x in self._path:
+            for node_y, node_x in self._path.memory:
                 x.append(node_x)
                 y.append(node_y)
 
             plt.plot(x, y, 'k')
-            start_row, start_column = self._path[0]
+            start_row, start_column = self._path.memory[0]
             goal_row, goal_column = self._goal_pos
             plt.scatter(x=[start_column, goal_column], y=[start_row, goal_row],
                         c='r', s=40, zorder=3)
             plt.show()
+            #display(plt.show())
 
         elif mode == 'rgb_array':
-            output = np.zeros((self._shape[0], self._shape[1], 3))
+            output = np.zeros((self._shape[0], self._shape[1], 3), dtype=np.uint8)
 
             # Set red channel to height
             output[:, :, 0] = self._grid
 
             # Scale red channel to be approximately 0 to 255
             max_value = np.amax(self._grid)
-            output[:, :, 0] /= max_value
+            min_value = np.amin(self._grid)
+            # output[:, :, 0] /= max_value
+            # output[:, :, 0] *= 255
+            
+            # Linear scale, in which every number is moved down
+            # by the lowest, so that lowest point lies at 0, and
+            # every point is linearly scaled by the factor needed
+            # to move the moved highest point to 255. The result is
+            # lowest point is at 0 and highest is at 255, and every
+            # other is somewhere inbetween
+            def scale_linear(highest, lowest, number):
+                return (number - lowest) * (255/(highest - lowest))
+
+            for y, row in enumerate(output[:, :, 0]):
+                for x, column in enumerate(row):
+                    output[y, x, 0] = scale_linear(max_value, min_value, column)
 
             # Set green pixel at start, blue pixel at goal
-            output[self._drone_pos[0] - 1, self._drone_pos[1] - 1, 1] = 1
-            output[self._goal_pos[0] - 1, self._goal_pos[1] - 1, 2] = 1
+            output[self._drone_pos[0] - 1, self._drone_pos[1] - 1, 1] = 255
+            output[self._goal_pos[0] - 1, self._goal_pos[1] - 1, 2] = 255
 
             return output
-        
+
         return None
 
+
 ACTION_LOOKUP = {
-    0 : (1, 0),
-    1 : (0, 1),
-    2 : (-1, 0),
-    3 : (0, -1)
+    0: (1, 0),
+    1: (0, 1),
+    2: (-1, 0),
+    3: (0, -1)
 }
